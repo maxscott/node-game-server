48e21d305bbe67fa6400980fb87249ed9a1e89e8
Max Weinbrown <mweinbrown@appnexus.com>
2015-07-10 18:31:54 -0400

    changed port

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use('/js', express.static(__dirname + '/js'));
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 1234);
